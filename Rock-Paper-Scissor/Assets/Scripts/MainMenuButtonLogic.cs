bacd3d43d9e62c22bb08021ceac727eb397881ef
a.aghighi <a.aghighi@medrick.info>
2024-09-27 05:32:02 +0330

    Created logic to handle some parts of the main menu tasks

---
+++ Rock-Paper-Scissor/Assets/Scripts/MainMenuButtonLogic.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonLogic : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _helpButton;
+    [SerializeField] private Button _quitButton;
+    [SerializeField] private GameObject _optionPanel;
+    [SerializeField] private GameObject _helpPanel;
+
+    [HideInInspector] public static MainMenuButtonLogic Instance { get; set; }
+    [HideInInspector] public event Action<GameObject , bool> OnPanelActivator;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("CoreScene");
+    }
+
+    public void Help()
+    {
+        OnPanelActivator?.Invoke(_helpPanel, true);
+    }
+
+    public void Option()
+    {
+        OnPanelActivator?.Invoke(_optionPanel, true);
+    }
+}
