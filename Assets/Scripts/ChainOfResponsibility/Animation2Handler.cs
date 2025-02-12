c4c127db17b8443a89ce61627de2a90787e13d95
a.aghighi <a.aghighi@medrick.info>
2025-02-12 23:00:19 +0330

    Created a chain of responsibility
     to handle all animations and core play one after another

---
+++ Assets/Scripts/ChainOfResponsibility/Animation2Handler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation2Handler : ChainOfResponsibility
+{
+    private Animator animator;
+
+    [SerializeField] private GameObject startCounter;
+    [SerializeField] private string triggerString;
+
+    private void Awake() => animator = startCounter.GetComponent<Animator>();
+
+    protected override IEnumerator Process()
+    {
+        animator.SetTrigger(triggerString);
+        yield return new WaitForSeconds(3f);
+        Continue();
+    }
+
+}
---
+++ Assets/Scripts/ChainOfResponsibility/Refree.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+
+public class Refree : MonoBehaviour
+{
+    public static Refree Instance { get; private set; }
+
+    [SerializeField] private GameObject refreeUI;
+
+    private int playerScore = 0;
+    private int opponentScore = 0;
+
+    public bool gameEnd = false;
+    public static bool isDraw = false;
+
+    public event Action<int, int> OnScoreUpdate;
+    public event Action<bool> OnSetPLayerAbilityToChooseHero;
+    public event Action<bool> OnShowResult;
+    public event Action OnGameStart;
+    public event Action OnGameStop;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        ChainOfResponsibility animation1 = refreeUI.GetComponent<Animation1Handler>();
+        ChainOfResponsibility animation2 = refreeUI.GetComponent<Animation2Handler>();
+        ChainOfResponsibility decision = refreeUI.GetComponent<DecisionHandler>();
+        ChainOfResponsibility result = refreeUI.GetComponent<ResultHandler>();
+
+        animation1.SetNext(animation2);
+        animation2.SetNext(decision);
+        decision.SetNext(result);
+        result.SetNext(animation1);
+
+        animation1.Execute();
+    }
+
+    private void Update()
+    {
+        if (gameEnd)
+        {
+            if (playerScore > opponentScore)
+                OnShowResult?.Invoke(true);
+            else
+                OnShowResult?.Invoke(false);
+        }
+    }
+
+    public void TriggerGameStart()
+    {
+        OnGameStart.Invoke();
+        isDraw = false;
+    }
+
+    public void AllowPlayerToSelect() => OnSetPLayerAbilityToChooseHero?.Invoke(true);
+
+    public void DisallowPlayerToSelect() => OnSetPLayerAbilityToChooseHero?.Invoke(false);
+
+
+    public void DetermineWinner(HeroScriptalbeObjects playerChoice, HeroScriptalbeObjects opponentChoice)
+    {
+        bool drawRound = playerChoice.hero_Catagory == opponentChoice.hero_Catagory;
+        bool playerWin = playerChoice.CanDefeatEnemyList.Contains(opponentChoice.hero_Catagory);
+
+        UpdateScore(playerWin, drawRound);
+    }
+
+    public void UpdateScore(bool playerWon, bool draw)
+    {
+        if (draw)
+        {
+            isDraw = true;
+        }
+        else if (playerWon)
+        {
+            playerScore++;
+        }
+        else
+        {
+            opponentScore++;
+        }
+
+        OnScoreUpdate?.Invoke(playerScore, opponentScore);
+
+        if (playerScore >= 3 || opponentScore >= 3)
+        {
+            gameEnd = true;
+            return;
+        }
+    }
+}
